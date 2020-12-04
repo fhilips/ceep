bdf94e8904b1473ffbd22546310027fc84d3970e
Filipe dos Santos <fhilips.souza@gmail.com>
2020-12-04 11:09:11 -0300

    adicionar estilos

---
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import CardNota from '../CardNota/CardNota';
+
+import './estilo.css';
+
+class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul className="lista-notas">
+                {Array.of("Trabalho", "Estudos", "Trabalho").map((categoria, index) => {
+                    return (
+                        <li className="lista-notas_item" key={index}>
+                            <div>{categoria}</div>
+                            <CardNota />
+                        </li>
+                        )
+                    })
+                }
+            </ul>
+        );
+    }
+
+}
+
+export default ListaDeNotas;
